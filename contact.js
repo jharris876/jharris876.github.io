4480107c352267e80684c9c4021e618b16678ceb
Jacob Harris <jacobh12jacob876@gmail.com>
2023-02-01 22:06:58 -0600

    Deleted commented out code

--- contact.js
+++ contact.js
@@ -1,28 +1,3 @@
-/*function sendMail(){
-    var params = {
-        name: document.getElementById("name").value , 
-        phone: document.getElementById("phone").value ,
-        email: document.getElementById("email").value ,
-        message: document.getElementById("message").value ,
-
-    };
-
-    const serviceID = "service_xnukp8n";
-    const templateID = "template_mv0tglo";
-
-    emailjs
-        .send(serviceID, templateID, params)
-        .then((res) => {
-            document.getElementById("name").value = "";
-            document.getElementById("phone").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("message").value = "";
-            console.log(res);
-            alert("Your message was sent successfully! Thank You!");
-        })
-        .catch((err) => console.log(err));
-}*/
-
 function sendMail(){
     event.preventDefault();
     var params = {
